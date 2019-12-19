7136cee53920fba77a506aa8a4bffe796bb4fe60
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2019-12-19 09:28:11 +0400

    tiny changes in mynumber.rb

--- day2/mynumber.rb
+++ day2/mynumber.rb
@@ -9,12 +9,10 @@ class MyNumber
 
 	def six_digit_palindrome
 		palindrome_6 = Integer(num_str + num_str.reverse)
-		palindrome_6
 	end
 
 	def five_digit_palindrome
 		palindrome_5 = Integer(num_str + num_str[1] + num_str[0])
-		palindrome_5
 	end
 
 	def three_digits_multiple?(number)
