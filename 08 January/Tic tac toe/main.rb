c51628acb186842574fbd368acffd9c7e998c848
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2020-01-08 20:47:53 +0400

    changed Tic tac toe/main.rb

--- 08 January/Tic tac toe/main.rb
+++ 08 January/Tic tac toe/main.rb
@@ -3,10 +3,10 @@ require_relative 'tic_tac_toe'
 ttt = TicTacToe.new()
 ttt.start
 while true
-	if ttt.first_player_turn == true
+	if ttt.first_player_turn
 		puts "#{ttt.first_player} won the match!"
 		break
-	elsif ttt.second_player_turn == true
+	elsif ttt.second_player_turn
 		puts "#{ttt.second_player} won the match!"
 		break
 	end
